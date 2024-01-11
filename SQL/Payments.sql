50296cdb1042e9b448e3ba6e609913d06c308075
AuroreTBF <archlee196@gmail.com>
2024-01-11 23:55:45 +0100

    database finished

--- SQL/Payments.sql
+++ SQL/Payments.sql
@@ -5,8 +5,7 @@ CREATE TABLE Transactions (
     payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     status VARCHAR(50),
     payment_method int(50),
-    FOREIGN KEY (shop_id) REFERENCES shopping_cart(Shopping_cart_id),
-    FOREIGN KEY (payment_method) REFERENCES PaymentMethods(Payment_Method_id)
+
 );
 CREATE TABLE PaymentMethods (
     Payment_Method_id INT PRIMARY KEY,
@@ -15,20 +14,17 @@ CREATE TABLE PaymentMethods (
 CREATE TABLE DebitPayment (
     Payment_id INT PRIMARY KEY,
     Payment_Method_id INT,
-    payment_amount DECIMAL(18, 2),
-    FOREIGN KEY (Payment_Method_id) REFERENCES PaymentMethods(Payment_Method_id)
+    payment_amount DECIMAL(18, 2)
 );
 CREATE TABLE CashPaymentDetails (
     cash_id INT PRIMARY KEY,
     payment_id INT,
-    cash_amount DECIMAL(18, 2),
-    FOREIGN KEY (payment_id) REFERENCES PaymentMethods(Payment_id)
+    cash_amount DECIMAL(18, 2)
 );
 CREATE TABLE PaypalDetails (
     paypal_id INT PRIMARY KEY,
     payment_id INT,
-    total_amount DECIMAL(18, 2),
-    FOREIGN KEY (payment_id) REFERENCES PaymentMethods(Payment_id)
+    total_amount DECIMAL(18, 2)
 );
 
 ALTER TABLE `debitpayment` CHANGE `Payment_id` `debit_id` INT(11) NOT NULL, CHANGE `Payment_Method_id` `payment_id` INT(11) NULL DEFAULT NULL, CHANGE `payment_amount` `total_amount` DECIMAL(18,2) NULL DEFAULT NULL;
--- SQL/orders.sql
+++ SQL/orders.sql
@@ -2,7 +2,5 @@ CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     product_id INT,
     order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    payment_id INT,
-    FOREIGN KEY (product_id) REFERENCES products(product_id),
-    FOREIGN KEY (payment_id) REFERENCES transactions(payment_id)
+    payment_id INT
 );
